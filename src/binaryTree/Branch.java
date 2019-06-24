0eeb8ff558da149ba11ac9cee78b9a6004fde9a6
stuser <stuser@192.168.2.97>
2019-06-24 11:12:35 -0400

    First Attempt - Binary Tree

--- src/binaryTree/Branch.java
+++ src/binaryTree/Branch.java
@@ -1,11 +1,11 @@
 package binaryTree;
 
 @SuppressWarnings("rawtypes")
-public class Branch/*<T extends Comparable>*/ {
-	private Object val;
+public class Branch<T extends Comparable<T>> {
+	private T val;
 	private Branch left;
 	private Branch right;
-	public Branch(Object obj)
+	public Branch(T obj)
 	{
 		this.val = obj;
 	}
@@ -17,7 +17,7 @@ public class Branch/*<T extends Comparable>*/ {
 	{
 		return right;
 	}
-	public Object getVal()
+	public T getVal()
 	{
 		return val;
 	}
@@ -28,5 +28,131 @@ public class Branch/*<T extends Comparable>*/ {
 	public void setRight(Branch br)
 	{
 		this.right = br;
+	}
+	public boolean hasLeft()
+	{
+		if (this.left == null)
+		{
+			return false;
+		}
+		return true;
+	}
+	public boolean hasRight()
+	{
+		if (this.right == null)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public void add(T arg0) 
+	{
+		if(val.compareTo(arg0) <= 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			try
+			{
+				if(!(this.getLeft() == null))
+				{
+					left.add(arg0);
+					return;
+				}
+				this.left = new Branch<T>(arg0);
+				return;
+			}
+			catch(NullPointerException e)
+			{
+				this.left = new Branch<T>(arg0);
+				return;
+			}
+		}	
+		if(val.compareTo(arg0) > 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			try
+			{
+				if(!(this.getRight() == null))
+				{
+					right.add(arg0);
+					return;
+				}
+				this.right = new Branch<T>(arg0);
+				return;
+			}
+			catch(NullPointerException e)
+			{
+				this.right = new Branch<T>(arg0);
+				return;
+			}
+		}
+	}
+	public boolean find(T arg0)
+	{
+		if(val.compareTo(arg0) < 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			try
+			{
+				return left.find(arg0);
+				
+			}
+			catch(NullPointerException e)
+			{
+				return false;
+			}
+		}
+		if(val.compareTo(arg0) > 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			try
+			{
+				return right.find(arg0);	
+			}
+			catch(NullPointerException e)
+			{
+				return false;
+			}
+		}
+		if(val.compareTo(arg0) == 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			return true;
+		}
+		return false;
+	}
+	public Branch remove(T arg0)
+	{
+		if(val.compareTo(arg0) < 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			return left.remove(arg0);
+		}
+		if(val.compareTo(arg0) > 0) // arg0 > val :: -1 | arg0 = val :: 0 | arg0 < val :: 1
+		{
+			return right.remove(arg0);	
+		}
+		if(!(this.hasRight()) && !(this.hasLeft()))
+		{
+			return null;
+		}
+		if(this.hasRight() && !(this.hasLeft()))
+		{
+			return this.getRight();
+		}
+		if(this.hasLeft() && !(this.hasRight()))
+		{
+			return this.getLeft();
+		}
+		if(this.hasLeft() && this.hasRight())
+		{
+			return this.getLeft().hooBoyHereWeGo(this);
+		}
+		return null;
+	}
+	public Branch hooBoyHereWeGo(Branch br)
+	{
+		if(this.hasRight())
+		{
+			return this.hooBoyHereWeGo(br);
+		}
+		else
+		{
+			return this;
+		}
 	}
 }
--- src/binaryTree/Tree.java
+++ src/binaryTree/Tree.java
@@ -5,145 +5,66 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class Tree/*<T extends Comparable>*/ implements List{
+public class Tree <T extends Comparable<T>>{ //implements List{
 	private Branch root;
-	@Override
-	public boolean add(Object arg0) {
-		// TODO Auto-generated method stub
-		if (root.getVal() == null)
-		{
-			root = new Branch(arg0);
-		}
-		return false;
-	}
-
-	@Override
-	public void add(int arg0, Object arg1) {
+	
+	public void add(T arg0) {
 		// TODO Auto-generated method stub
 		
+		if (root == null)
+		{
+			this.root = new Branch<T>(arg0);
+			return;
+		}
+		root.add(arg0);
 	}
 
-	@Override
-	public boolean addAll(Collection arg0) {
+	public void addAll(Collection<T> arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		for (T val : arg0)
+		{
+			root.add(val);
+		}
 	}
 
-	@Override
-	public boolean addAll(int arg0, Collection arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
+		root = null;
+	}
+
+	public boolean contains(T arg0) {
+		// TODO Auto-generated method stub
+		return root.find(arg0);
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		if (root == null)
+			return true;
+		return false;
+	}
+
+	public boolean remove(T o) {
+		// TODO Auto-generated method stub
+		if(!(this.contains(o)))
+		{
+			return false;	
+		}
+		root.remove(o);
+			
 		
 	}
 
-	@Override
-	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ListIterator listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean removeAll(Collection c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object set(int index, Object element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
-	public List subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Object[] toArray(Object[] a) {
 		// TODO Auto-generated method stub
 		return null;
